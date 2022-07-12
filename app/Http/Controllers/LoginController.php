179a50b9b2d989aaa5896c5a6a67a4cd5dce228d
aceng26 <tupirasa22@gmail.com>
2022-07-12 11:25:05 +0700

    Menambahkan middleware

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function halamanlogin(){
+        return view('Login.Login-aplikasi');
+    }
+
+
+    public function postlogin(Request $request){
+        if(Auth::attempt($request->only('email','password'))){
+            return redirect('/home');
+        }
+        return redirect('/login');
+    }
+
+    public function logout(){
+      Auth::logout();
+      return redirect('/login');
+    }
+}
--- resources/views/Template/sidebar.blade.php
+++ resources/views/Template/sidebar.blade.php
@@ -23,6 +23,9 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
+            
+          @if (auth()->user()->level == "karyawan")
+
           <li class="nav-item has-treeview">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-fingerprint"></i>
@@ -46,6 +49,7 @@
               </li>
             </ul>
           </li>
+              @endif
 
           <li class="nav-item has-treeview">
             <a href="#" class="nav-link">
@@ -56,12 +60,16 @@
               </p>
             </a>
             <ul class="nav nav-treeview">
+              @if (auth()->user()->level == "karyawan")
               <li class="nav-item">
                 <a href="#" class="nav-link">
                   <i class="fas fa-user-alt nav-icon"></i>
                   <p>Laporan Perkaryawan</p>
                 </a>
               </li>
+               @endif
+
+               @if (auth()->user()->level == "admin")
               <li class="nav-item">
                 <a href="#" class="nav-link">
                   <i class="fas fa-users nav-icon"></i>
@@ -70,6 +78,18 @@
               </li>
             </ul>
           </li>
+              @endif
+
+
+          <li class="nav-item">
+            <a href="{{ route('logout') }}" class="nav-link">
+              <i class="fas fa-sign-out-alt nav-icon"></i>
+              <p>Logout</p>
+            </a>
+          </li>
+        </ul>
+      </li>
+
 
         </ul>
       </nav>
